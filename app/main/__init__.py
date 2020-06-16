1cbe9d819666335de10a1961cf3d324b03eee792
Roman Voronkin <roman.voronkin@gmail.com>
2020-06-16 20:38:49 +0300

    Добавлено описание дней недели в контекст шаблонов приложения

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,4 +4,10 @@ from flask import Blueprint
 main = Blueprint('main', __name__)
 
 
+@main.app_context_processor
+def inject_weekdays():
+    from data import weekdays
+    return dict(weekdays=weekdays)
+
+
 from . import errors, filters, views  # noqa
--- data/__init__.py
+++ data/__init__.py
@@ -1,1 +1,15 @@
+from collections import namedtuple
+
 from .data import goals, teachers  # noqa
+
+Weekday = namedtuple('Weekday', ['slug', 'title'])
+
+weekdays = (
+    Weekday(slug="mon", title="Понедельник"),
+    Weekday(slug="tue", title="Вторник"),
+    Weekday(slug="wed", title="Среда"),
+    Weekday(slug="thu", title="Четверг"),
+    Weekday(slug="fri", title="Пятница"),
+    Weekday(slug="sat", title="Суббота"),
+    Weekday(slug="sun", title="Воскресение"),
+)
